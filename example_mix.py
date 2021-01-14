e3e79880512a1739a9c24eda0be76dfbb6cf21f6
Marc <pachacamac@users.noreply.github.com>
2021-01-15 00:13:53 +0100

    Update example_mix.py

--- example_mix.py
+++ example_mix.py
@@ -1,8 +1,8 @@
-from mixer import *
+from mixer import Track, soundLib
 
 lib = soundLib("""
-https://freesound.org/people/InspectorJ/sounds/393878/ can_open
-https://freesound.org/people/craigglenday/sounds/517173/ drinking
+can_open  https://freesound.org/people/InspectorJ/sounds/393878/
+drinking  https://freesound.org/people/craigglenday/sounds/517173/
 """, "samples")
 
 track = Track()
@@ -10,6 +10,6 @@ track.add(lib.can_open[:2000].stretch(1.2).stereo_pan())
 track.add(lib.drinking.stretch(1.2).stereo_pan(-1), track.t-1000)
 print(track.duration())
 mixed = track.mix()
-show(mixed)
-play(mixed)
+mixed.show()
+mixed.play()
 #mixed.export("example.mp3", format="mp3", tags={'artist': 'The Coders', 'album': 'Example Mix', 'comments': 'Example'}))
