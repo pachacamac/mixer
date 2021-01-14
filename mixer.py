627f6b52250d5ada77b6645cc841f09f0f695deb
Marc <pachacamac@users.noreply.github.com>
2021-01-14 02:26:20 +0100

    Update mixer.py

--- mixer.py
+++ mixer.py
@@ -6,6 +6,38 @@ import scipy.interpolate as interp
 import os, glob, array
 from pathlib import Path
 
+class LazySoundLibDict(dict):
+  def setPath(self, path):
+    self.__path__ = path
+  def __getitem__(self, key):
+    item = dict.__getitem__(self, key)
+    if key == '__path__':
+      return item
+    if isinstance(item, str):
+      print(f'loading {key} ...')
+      item = fetchAndLoad(item, key, self.__path__)
+      dict.__setitem__(self, key, item)
+    return item
+  def __getattr__(self, key):
+    try:
+      return self.__getitem__(key)
+    except KeyError as k:
+      raise AttributeError(k)
+  def __setattr__(self, key, value):
+    dict.__setitem__(self, key, value)
+  def __delattr__(self, key):
+    try:
+      del self[key]
+    except KeyError as k:
+      raise AttributeError(k)
+
+def soundLib(str, path='.'):
+  lib = LazySoundLibDict()
+  lib.setPath(path)
+  for url, name in [s.split(' ', 1) for s in str.strip().split("\n")]:
+    lib[name] = url
+  return lib
+
 def load(path, lib=None, name=None):
   formats = dict(aiff="aac")
   format = path.split('.')[-1]
@@ -40,14 +72,22 @@ def loadAll(path, lib={}):
     load(str(path), lib)
   return lib
 
-def normalize_duration(n) -> int:
-  return int(n*1000) if isinstance(n, float) else n
+# mm:ss:mmmm
+def parseTime(s) -> int:
+  s = str(s)
+  nums = [int(n) for n in s.split(':')]
+  if len(nums) == 1: # ms
+    return nums[0]
+  elif len(nums) == 2: # ss:iiii
+    return nums[0]*1000 + nums[1]
+  elif len(nums) == 3: # mm:ss:iiii
+    return nums[0]*60*1000+nums[1]*1000 + nums[2]
 
 def silence(duration) -> AudioSegment:
-  return AudioSegment.silent(normalize_duration(duration))
+  return AudioSegment.silent(duration)
 
 def whitenoise(duration) -> AudioSegment:
-  WhiteNoise().to_audio_segment(duration=normalize_duration(duration)).apply_gain(-40)
+  WhiteNoise().to_audio_segment(duration=duration).apply_gain(-40)
 
 def show(snd: AudioSegment, width=120, skip_print=False) -> str:
   str = ''
@@ -73,28 +113,38 @@ def showMono(snd: AudioSegment, width=120, skip_print=False) -> str:
   return str
 
 def play(snd: AudioSegment, start=0, end=None, duration=None):
-  start = normalize_duration(start)
   if end is not None:
-    sound = snd[start:normalize_duration(end)]
+    sound = snd[start:end]
   elif duration is not None:
-    sound = snd[start:start+abs(normalize_duration(duration))]
+    sound = snd[start:start+abs(duration)]
   else:
     sound = snd[start:None]
   playback(sound)
 
-def add(sound: AudioSegment, samples=[], position=0) -> list:
-  samples.append(dict(sound=sound, position=normalize_duration(position)))
-  samples.sort(key=lambda item: item.get("position"))
-  return position + len(sound)
+class Track(list):
+  def __init__(self):
+    self.t = 0
 
-def mix(samples=[], normalize=True) -> AudioSegment:
-  samples.sort(key=lambda item: item.get("position"))
-  length = samples[-1]['position'] + len(samples[-1]['sound'])
-  base = silence(length)
-  for sample in samples:
-    sound = sample['sound'].normalize() if normalize else sample['sound']
-    base = base.overlay(sound, position=sample['position'])
-  return base
+  def duration(self):
+    last = self[-1]
+    return last['position'] + len(last['sound'])
+
+  def add(self, sound: AudioSegment, position=None):
+    if position is None:
+      position = self.t
+    self.append(dict(sound=sound, position=position))
+    self.sort(key=lambda item: item.get("position"))
+    self.t = position + len(sound)
+    return self.t
+
+  def mix(self, normalize=False) -> AudioSegment:
+    self.sort(key=lambda item: item.get("position"))
+    length = self[-1]['position'] + len(self[-1]['sound'])
+    base = silence(length)
+    for sample in self:
+      sound = sample['sound'].normalize() if normalize else sample['sound']
+      base = base.overlay(sound, position=sample['position'])
+    return base
 
 # Monkey patching AudioSegment
 def as_echo(self, delay=300, repeats=3, gain_change=-3) -> AudioSegment:
@@ -132,3 +182,21 @@ def as_stretch(self, factor) -> AudioSegment:
     channels[i] = chan._spawn(array.array('h', c))
   return AudioSegment.from_mono_audiosegments(*channels)
 AudioSegment.stretch = as_stretch
+
+# TODO: 
+def as_trim_silence(self, threshold=0) -> AudioSegment:
+  # channels = self.split_to_mono()
+  # lmin=0
+  # rmax=len(self)
+  # for i, chan in enumerate(channels):
+  #   for n in chan.get_array_of_samples():
+  pass
+
+AudioSegment.trim_silence = as_trim_silence
+
+#played_togther = sound1.overlay(sound2)
+#sound2_starts_after_delay = sound1.overlay(sound2, position=5000)
+#volume_of_sound1_reduced_during_overlay = sound1.overlay(sound2, gain_during_overlay=-8)
+#sound2_repeats_until_sound1_ends = sound1.overlay(sound2, loop=true)
+#sound2_plays_twice = sound1.overlay(sound2, times=2)
+  
